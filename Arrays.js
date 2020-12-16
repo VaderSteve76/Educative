2ec5a9cad96b917c75b350ae54b0e1d5d0f52c7d
Steve <alverson.s@yahoo.com>
2020-12-15 19:40:18 -0500

    Rotate array by n elements solution 1

--- Arrays.js
+++ Arrays.js
@@ -161,3 +161,42 @@ console.log("Least Common Number: " + findLeastCommonNumber(v1, v2, v3));
 
 
 //////////////////////////////////////////////////////
+
+// Rotate an Array by N Elements
+let reverseArray = function(arr, start, end) {
+  while(start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+
+let rotateArray = function(arr, n) {
+  let len = arr.length;
+  // normalize rotations if n > arr size or n is -negative
+  n = n % len;
+  if(n < 0) {
+    // calculate the positive rotations needed
+    n = n + len;
+  }
+  // partition the arr based on rotations 'n'
+  // ex: 1, 2, 3, 4, 5 where n = 2.
+  // -> 5, 4, 3, 2, 1
+  // -> 4, 5, 3, 2, 1
+  // -> 4, 5, 1, 2, 3
+  reverseArray(arr, 0, len - 1);
+  reverseArray(arr, 0, n - 1);
+  reverseArray(arr, n, len - 1);
+}
+
+let arr = [1, 10, 20, 0, 59, 86, 32, 11, 9, 40];
+
+console.log("Array Before Rotation");
+console.log(arr);
+
+rotateArray(arr, 2);
+
+console.log("Array After Rotation");
+console.log(arr);
